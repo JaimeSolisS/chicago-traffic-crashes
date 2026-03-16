b2c36717fb7272840fd3d74c804e7dc551358c11
JaimeSolisS <jaimesolissoto@me.com>
2026-03-15 20:45:53 -0600

    feat(transformation): add casting and drop null columns from raw models

--- transform/models/raw/raw_crashes.sql
+++ transform/models/raw/raw_crashes.sql
@@ -1,6 +1,52 @@
 with source as (
     select
-        *
+        --Left out columns that are mostly NULL
+        crash_record_id as crash_record_id,
+        --crash_date_est_i as crash_date_est_i,
+        crash_date as crash_date,
+        cast(posted_speed_limit as numeric) as posted_speed_limit,
+        traffic_control_device as traffic_control_device,
+        device_condition as device_condition,
+        weather_condition as weather_condition,
+        lighting_condition as lighting_condition,
+        first_crash_type as first_crash_type,
+        trafficway_type as trafficway_type,
+        alignment as alignment,
+        roadway_surface_cond as roadway_surface_condition,
+        road_defect as road_defect,
+        report_type as report_type,
+        crash_type as crash_type,
+        --intersection_related_i as intersection_related_i,
+        --private_property_i as private_property_i,
+        hit_and_run_i as hit_and_run_i,
+        damage as damage,
+        date_police_notified as date_police_notified,
+        prim_contributory_cause as prim_contributory_cause,
+        sec_contributory_cause as sec_contributory_cause,
+        cast(street_no as numeric) as street_number,
+        street_direction as street_direction,   
+        street_name as street_name,
+        cast(beat_of_occurrence as numeric) as beat_of_occurrence,
+        --photos_taken_i as photos_taken_i,
+        --statements_taken_i as statements_taken_i,
+        --dooring_i as dooring_i,
+        --work_zone_i as work_zone_i,
+        --work_zone_type as work_zone_type,
+        cast(num_units as integer) as num_units,
+        most_severe_injury as most_severe_injury,
+        cast(injuries_total as integer) as injuries_total,
+        cast(injuries_fatal as integer) as injuries_fatal,
+        cast(injuries_incapacitating as integer) as injuries_incapacitating,
+        cast(injuries_non_incapacitating as integer) as injuries_non_incapacitating,
+        cast(injuries_reported_not_evident as integer) as injuries_reported_not_evident,
+        cast(injuries_no_indication as integer) as injuries_no_indication,
+        cast(injuries_unknown as integer) as injuries_unknown,
+        cast(crash_hour as integer) as crash_hour,
+        cast(crash_day_of_week as integer) as crash_day_of_week,
+        cast(crash_month as integer) as crash_month,
+        cast(latitude as numeric) as latitude,
+        cast(longitude as numeric) as longitude
+        --location__type as location_type
     from {{ source('raw_data', 'external_crashes') }}
 )
 
--- transform/models/raw/raw_people.sql
+++ transform/models/raw/raw_people.sql
@@ -1,6 +1,33 @@
 with source as (
     select
-        *
+        --Left out columns that are mostly NULL
+        person_id as person_id,
+        person_type as person_type,
+        crash_record_id as crash_record_id,
+        vehicle_id  as vehicle_id,
+        crash_date as crash_date, 
+        --seat_no as seat_no,
+        city as city,
+        state as state,
+        zipcode as zipcode,
+        sex as sex, 
+        cast(age as integer) as age,
+        drivers_license_state as drivers_license_state,
+        drivers_license_class as drivers_license_class,
+        safety_equipment as safety_equipment,
+        airbag_deployed as airbag_deployed,
+        ejection as ejection, 
+        injury_classification as injury_classification, 
+        --hospital as hospital, 
+        --ems_agency as ems_agency, 
+        --ems_run_no as ems_run_no, 
+        driver_action as driver_action,
+        driver_vision as driver_vision, 
+        physical_condition as physical_condition, 
+        --pedpedal_action as pedpedal_action,
+        --pedpedal_visibility as pedpedal_visibility, 
+        --pedpedal_location as pedpedal_location, 
+        bac_result as bac_result
     from {{ source('raw_data', 'external_people') }}
 )
 
