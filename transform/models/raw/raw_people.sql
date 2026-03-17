11baf9e17ebe41fd8cc032453597bf6c9289988e
JaimeSolisS <jaimesolissoto@me.com>
2026-03-16 18:19:19 -0600

    chore(transformation): group columns

--- transform/models/raw/raw_people.sql
+++ transform/models/raw/raw_people.sql
@@ -1,34 +1,40 @@
-with source as (
-    select
+WITH source AS (
+    SELECT
         --Left out columns that are mostly NULL
+        -- IDENTITY --
         person_id as person_id,
         person_type as person_type,
         crash_record_id as crash_record_id,
-        vehicle_id  as vehicle_id,
-        crash_date as crash_date, 
-        --seat_no as seat_no,
+        vehicle_id as vehicle_id,
+        crash_date as crash_date,
+        -- DEMOGRAPHICS --
+        sex as sex,
+        CAST(age as integer) as age,
         city as city,
         state as state,
         zipcode as zipcode,
-        sex as sex, 
-        cast(age as integer) as age,
+        -- DRIVER INFO --
         drivers_license_state as drivers_license_state,
         drivers_license_class as drivers_license_class,
+        driver_action as driver_action,
+        driver_vision as driver_vision,
+        physical_condition as physical_condition,
+        -- SAFETY & INJURY --
+        --seat_no as seat_no,
         safety_equipment as safety_equipment,
         airbag_deployed as airbag_deployed,
-        ejection as ejection, 
-        injury_classification as injury_classification, 
-        --hospital as hospital, 
-        --ems_agency as ems_agency, 
-        --ems_run_no as ems_run_no, 
-        driver_action as driver_action,
-        driver_vision as driver_vision, 
-        physical_condition as physical_condition, 
+        ejection as ejection,
+        injury_classification as injury_classification,
+        bac_result as bac_result,
+        --hospital as hospital,
+        --ems_agency as ems_agency,
+        --ems_run_no as ems_run_no,
+        -- PEDESTRIAN/CYCLIST --
         --pedpedal_action as pedpedal_action,
-        --pedpedal_visibility as pedpedal_visibility, 
-        --pedpedal_location as pedpedal_location, 
-        bac_result as bac_result
-    from {{ source('raw_data', 'external_people') }}
+        --pedpedal_visibility as pedpedal_visibility,
+        --pedpedal_location as pedpedal_location
+
+    FROM {{ source('raw_data', 'external_people') }}
 )
 
-select * from source
+SELECT * FROM source
--- transform/models/raw/raw_vehicle.sql
+++ transform/models/raw/raw_vehicle.sql
@@ -1,27 +1,30 @@
-with source as (
-    select
+WITH source AS (
+    SELECT
         --Left out columns that are mostly NULL
-        cast(crash_unit_id as numeric) as crash_unit_id,
+        -- IDENTITY --
+        CAST(crash_unit_id as numeric) as crash_unit_id,
+        CAST(unit_no as numeric) as unit_no,
+        unit_type as unit_type,
         crash_record_id as crash_record_id,
         crash_date as crash_date,
-        cast(unit_no as numeric) as unit_no,
-        unit_type as unit_type,
-        cast(num_passengers as integer) as num_passengers,
-        cast(vehicle_id as numeric) as vehicle_id,
-        --cmrc_veh_i as cmrc_veh_i,
+        CAST(vehicle_id as numeric) as vehicle_id,
+        -- VEHICLE INFO --
         make as make,
         model as model,
-        lic_plate_state as lic_plate_state,
-        cast(vehicle_year as integer) as vehicle_year,
-        vehicle_defect as vehicle_defect,
+        CAST(vehicle_year as integer) as vehicle_year,
         vehicle_type as vehicle_type,
         vehicle_use as vehicle_use,
+        vehicle_defect as vehicle_defect,
+        lic_plate_state as lic_plate_state,
+        --cmrc_veh_i as cmrc_veh_i,
+        -- OCCUPANTS --
+        CAST(num_passengers as integer) as num_passengers,
+        CAST(occupant_cnt as integer) as occupant_cnt,
+        -- MOVEMENT --
         travel_direction as travel_direction,
         maneuver as maneuver,
-        --towed_i as towed_i,
-        cast(occupant_cnt as integer) as occupant_cnt,
-        --towed_by as towed_by,
-        --towed_to as towed_to,
+        -- CRASH INVOLVEMENT --
+        first_contact_point as first_contact_point,
         --area_00_i as area_00_i,
         --area_01_i as area_01_i,
         --area_02_i as area_02_i,
@@ -36,32 +39,36 @@ with source as (
         --area_11_i as area_11_i,
         --area_12_i as area_12_i,
         --area_99_i as area_99_i,
-        first_contact_point as first_contact_point,
+        --towed_i as towed_i,
+        --towed_by as towed_by,
+        --towed_to as towed_to,
+        -- Commercial vehicle (mostly NULL)
         --cast(cmv_id as numeric) as cmv_id,
         --usdot_no as usdot_no,
         --ccmc_no as ccmc_no,
         --commercial_src as commercial_src,
         --gvwr as gvwr,
-        --carrier_name as carrier_name, 
+        --vehicle_config as vehicle_config,
+        --cargo_body_type as cargo_body_type,
+        --load_type as load_type,
+        --cast(axle_cnt as integer) as axle_cnt,
+        --idot_permit_no as idot_permit_no,
+        --trailer1_width as trailer1_width,
+        --trailer2_width as trailer2_width,
+        --carrier_name as carrier_name,
         --carrier_state as carrier_state,
         --carrier_city as carrier_city,
         --hazmat_present_i as hazmat_present_i,
         --hazmat_report_i as hazmat_report_i,
         --hazmat_report_no as hazmat_report_no,
-        --mcs_report_i as mcs_report_i,
         --hazmat_vio_cause_crash_i as hazmat_vio_cause_crash_i,
+        --hazmat_out_of_service_i as hazmat_out_of_service_i,
+        --hazmat_class as hazmat_class,
+        --mcs_report_i as mcs_report_i,
         --mcs_vio_cause_crash_i as mcs_vio_cause_crash_i,
-        --idot_permit_no as idot_permit_no,
-        --trailer1_width as trailer1_width,
-        --trailer2_width as trailer2_width,
-        --cast(axle_cnt as integer) as axle_cnt,
-        --vehicle_config as vehicle_config,
-        --cargo_body_type as cargo_body_type,
-        --load_type as load_type,
-       --hazmat_out_of_service_i as hazmat_out_of_service_i,
-        --mcs_out_of_service_i as mcs_out_of_service_i,
-        --hazmat_class as hazmat_class
-    from {{ source('raw_data', 'external_vehicles') }}
+        --mcs_out_of_service_i as mcs_out_of_service_i
+
+    FROM {{ source('raw_data', 'external_vehicles') }}
 )
 
-select * from source
+SELECT * FROM source
