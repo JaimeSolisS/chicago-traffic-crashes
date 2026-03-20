bf2f7de86d3732b98d9416b1d3b54e382b2ac1a7
JaimeSolisS <jaimesolissoto@me.com>
2026-03-19 23:35:06 -0600

    chore(orchestration): add local pipeline script

--- orchestration/local/pipeline.py
+++ orchestration/local/pipeline.py
@@ -1,8 +1,8 @@
 import subprocess
 from datetime import datetime, timedelta
 
-start_date = datetime(2025, 1, 2)
-end_date = datetime(2026, 1, 1)
+start_date = datetime(2025, 9, 2)
+end_date = datetime(2025, 9, 3)
 
 current = start_date
 
@@ -13,15 +13,28 @@ while current <= end_date:
     print(f"--- CHICAGO TO MOTHERDUCK ---")
     subprocess.run(
         ["uv", "run", "chicago_to_motherduck/pipeline.py", date_str],
-        check=True
+        check=True,
+        cwd="../../ingestion"
     )
 
     print(f"--- MOTHERDUCK TO GCS ---")
     subprocess.run(
         ["uv", "run", "motherduck_to_gcs/pipeline.py", date_str],
-        check=True
+        check=True,
+        cwd="../../ingestion"
+    )
+
+    # This can be run outside the loop, so dbt only runs once, 
+    # but this is how the pipeline should work on a daily basis.
+    print("--- RUN DBT ---")
+    subprocess.run(
+        ["make", "dbt-run"],
+        check=True,
+        cwd="../.."
     )
 
     current += timedelta(days=1)
+
+
 
 print("All dates completed.")
